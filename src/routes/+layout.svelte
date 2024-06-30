3a569e4e4473bf9392838444e871a992cc4dacad
iamthe-Wraith <allhailthewraith@protonmail.com>
2024-06-30 17:50:54 -0400

    feat: adds styling

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import './global.css';
+</script>
+
+<main>
+    <slot></slot>
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,11 +8,67 @@
     <title>Jobber</title>
 </svelte:head>
 
-{#each data.resources as resource}
-    <a href={resource.url} target="_blank">
-        <div>
-            <h2>{resource.name}</h2>
-            <p>{resource.description}</p>
-        </div>
-    </a>
-{/each}
+<div class="container">
+    <h1>Find Your Next Job!</h1>
+
+    <p>Below is a collection of job hunting resources I've collected and wanted to share with people like you.</p>
+    <p>Good lunch in your job search!</p>
+
+    <div class="resources">
+        {#each data.resources as resource}
+            <a
+                href={resource.url}
+                target="_blank"
+                class="resource"
+            >
+                <p class="h5">{resource.name}</p>
+                <p>{resource.description}</p>
+            </a>
+        {/each}
+    </div>
+</div>
+
+<style>
+    .container {
+        width: 94vw;
+        max-width: 1200px;
+        margin: 0 auto;
+        padding: 2rem 0 1rem;
+
+        & p {
+            text-align: center;
+        }
+    }
+
+    .resources {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+        gap: 1rem;
+        margin-top: 2rem;
+    }
+
+    .resource {
+        margin-bottom: 1rem;
+        padding: 1rem;
+        border: 1px solid var(--neutral-200);
+        border-radius: 0.5rem;
+        background-color: var(--neutral-100);
+        text-decoration: none;
+        transition: 0.15s ease-in-out;
+
+        &:hover,
+        &:focus {
+            border: 1px solid var(--neutral-300);
+            background-color: var(--neutral-200);
+        }
+    }
+
+    .resource h2 {
+        margin-bottom: 0.5rem;
+    }
+
+    .resource p {
+        margin: 0;
+        text-align: center;
+    }
+</style>
